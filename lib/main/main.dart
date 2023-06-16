d42f75fc5f46985a6131097fdfa7dbfee2c4f34b
France <franciscodomingosjose06798@gmail.com>
2023-06-16 08:24:20 +0100

    fix: main path

---
+++ lib/main/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+
+import '../ui/components/makeAppTheme.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+    final routeObserver = Get.put<RouteObserver>(RouteObserver<PageRoute>());
+
+    return GetMaterialApp(
+      title: 'Booking Developers',
+      debugShowCheckedModeBanner: false,
+      theme: makeAppTheme(),
+      navigatorObservers: [routeObserver],
+      initialRoute: '/',
+      getPages: [],
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:booking_developers/main.dart';
+import 'package:booking_developers/main/main.dart';
 
 void main() {}
