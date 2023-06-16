e9cee40aa8821014de2330d2710222713aa67df1
France <franciscodomingosjose06798@gmail.com>
2023-06-16 08:13:11 +0100

    finesh main implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget {}
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+    final routeObserver = Get.put<RouteOberser>(RouteObserver<PageRoute>());
+
+    return GetMaterialApp(
+      title: 'Booking Developers',
+      debugShowCheckedModeBanner: false,
+      theme: makeAppTheme(),
+      navigatorObservers: [routeObserver],
+      initialRoute: '/',
+      getPages: [],
+    );
+  }
+}
