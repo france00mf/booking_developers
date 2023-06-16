73d5a78ab05d2109e321caf2a617ede20a95247a
France <franciscodomingosjose06798@gmail.com>
2023-06-16 08:22:16 +0100

    apply makeAppTheme Structure

--- lib/ui/components/makeAppTheme.dart
+++ lib/ui/components/makeAppTheme.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
 ThemeData makeAppTheme() {
+  final primaryColor = Colors.fromRGBO();
+  final textTheme = TextTheme();
+  final inputDecorationTheme = InputDecorationTheme();
+  final buttonTheme = ButtonThemeData();
+
   return ThemeData();
 }
