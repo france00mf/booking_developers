ab2d847f3715128c283a9fb248b7ab5181d336ce
France <franciscodomingosjose06798@gmail.com>
2023-06-19 13:24:11 +0100

    add all color refs

--- lib/ui/components/app_theme.dart
+++ lib/ui/components/app_theme.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 
 ThemeData makeAppTheme() {
   final primaryColor = Colors.fromRGBO();
+  final primaryColorDark;
+  final primaryColorLight;
+  final secondaryColor;
+  final secondaryClorDark;
+  final disabledColor;
+  final dividerColor;
   final textTheme = TextTheme();
   final inputDecorationTheme = InputDecorationTheme();
   final buttonTheme = ButtonThemeData();
