841983805ac44f228c19f136423b2edd760fa788
France <franciscodomingosjose06798@gmail.com>
2023-06-19 13:35:59 +0100

    add ButtonTheme configs

--- lib/ui/components/app_theme.dart
+++ lib/ui/components/app_theme.dart
@@ -17,7 +17,15 @@ ThemeData makeAppTheme() {
       focusedBorder: UnderlineInputBorder(
           borderSide: BorderSide(color: primaryColorLight)),
       alignLabelWithHint: true);
-  final buttonTheme = ButtonThemeData();
+  final buttonTheme = ButtonThemeData(
+      colorScheme: ColorScheme.light(
+        primary: primaryColor,
+      ),
+      buttonColor: primaryColor,
+      splashColor: primaryColorLight,
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      textTheme: ButtonTextTheme.primary,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)));
 
   return ThemeData();
 }
