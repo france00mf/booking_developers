549193c5c0d39b6c6c2065a71d29fdd187962187
France <franciscodomingosjose06798@gmail.com>
2023-06-19 13:26:11 +0100

    atribute all colorr for ui.

--- lib/ui/components/app_theme.dart
+++ lib/ui/components/app_theme.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 ThemeData makeAppTheme() {
-  final primaryColor = Colors.fromRGBO();
-  final primaryColorDark;
-  final primaryColorLight;
-  final secondaryColor;
-  final secondaryClorDark;
-  final disabledColor;
-  final dividerColor;
+  final primaryColor = Color.fromRGBO(136, 14, 79, 1);
+  final primaryColorDark = Color(0xFF600027);
+  final primaryColorLight = Color.fromRGBO(188, 71, 123, 1);
+  final secondaryColor = Color.fromRGBO(0, 77, 64, 1);
+  final secondaryClorDark = Color.fromRGBO(0, 37, 26, 1);
+  final disabledColor = Colors.grey[400];
+  final dividerColor = Colors.grey;
   final textTheme = TextTheme();
   final inputDecorationTheme = InputDecorationTheme();
   final buttonTheme = ButtonThemeData();
