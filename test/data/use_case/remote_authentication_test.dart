afbafc829de31638a2feafe70a6818ce72124e3b
France <franciscodomingosjose06798@gmail.com>
2023-06-20 15:42:06 +0100

    fix: Error impor on Mockit , teste , and faker

--- test/data/use_case/remote_authentication_test.dart
+++ test/data/use_case/remote_authentication_test.dart
@@ -1,4 +1,6 @@
+import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
