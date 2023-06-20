fd9316ba9bdec3fa5beccd0cd6da513b38d35117
France <franciscodomingosjose06798@gmail.com>
2023-06-20 16:08:40 +0100

    create RemoteAuthentication class and implement them

--- lib/data/use_case/remote_authentication.dart
+++ lib/data/use_case/remote_authentication.dart
@@ -1,1 +1,12 @@
-class RemoteAuthentication {}
+import 'package:booking_developers/data/http/http_client.dart';
+import 'package:booking_developers/domain/entity/account_entity.dart';
+import 'package:booking_developers/domain/use_case/authentication.dart';
+import 'package:meta/meta.dart';
+
+class RemoteAuthentication implements Authentication {
+  final HttpClient? httpClient;
+  final String? url;
+
+  RemoteAuthentication({@required this.httpClient, @required this.url});
+  Future<AccountEntity> auth(AuthenticationParams params) {}
+}
