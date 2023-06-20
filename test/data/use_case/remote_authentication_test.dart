a5eaf14cd0c077de9dc6db3c997777cfb7ac04eb
France <franciscodomingosjose06798@gmail.com>
2023-06-20 15:32:48 +0100

    add principal variables on setup

--- test/data/use_case/remote_authentication_test.dart
+++ test/data/use_case/remote_authentication_test.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
+
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
@@ -5,6 +7,14 @@ void main() {
   HttpClientSpy httpClient;
   String url;
   AuthenticatinParams params;
+
+  setUp(() {
+    sut = RemoteAuthenticationParams();
+    httpClient = HttpClientSpy();
+    params = AuthenticationParams(
+        email: faker.internet.email(), secret: faker.internet.password());
+    url = faker.internet.httpUrl();
+  });
 
   test('', () {});
 }
