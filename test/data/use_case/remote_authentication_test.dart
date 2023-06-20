efee62c21258e2bb2a16278a4db94959e3ffeb32
France <franciscodomingosjose06798@gmail.com>
2023-06-20 15:29:50 +0100

    add principal variables for remoteauthentication test

--- test/data/use_case/remote_authentication_test.dart
+++ test/data/use_case/remote_authentication_test.dart
@@ -1,5 +1,10 @@
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
+  RemoteAuthentication sut;
+  HttpClientSpy httpClient;
+  String url;
+  AuthenticatinParams params;
+
   test('', () {});
 }
