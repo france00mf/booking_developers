b3d27918c4cabb5dff4f6ff39360251947f6c94a
France <franciscodomingosjose06798@gmail.com>
2023-06-20 16:13:39 +0100

    finesh remoteauthenticationparams class and convert to Domain and json

--- lib/data/use_case/remote_authentication.dart
+++ lib/data/use_case/remote_authentication.dart
@@ -8,5 +8,20 @@ class RemoteAuthentication implements Authentication {
   final String? url;
 
   RemoteAuthentication({@required this.httpClient, @required this.url});
-  Future<AccountEntity> auth(AuthenticationParams params) {}
+  Future<AccountEntity> auth(AuthenticationParams params) async {
+    final httpResponse =
+        await httpClient!.request(url: url!, method: 'post', body: body);
+  }
+}
+
+class RemoteAuthenticationParams {
+  final String? email;
+  final String? password;
+
+  RemoteAuthenticationParams({@required this.email, @required this.password});
+
+  factory RemoteAuthenticationParams.fromDomain(AuthenticationParams params) =>
+      RemoteAuthenticationParams(email: params.email, password: params.secrect);
+
+  Map toJson() => {'email': email, 'password': password};
 }
